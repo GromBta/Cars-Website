9be9a55e6e8716b9044e732e618c2b17f30c544d
Ilyas Galiev <iljas-galiev@mail.ru>
2020-12-07 12:45:44 +0300

    registration page

--- Front-end/js/site.js
+++ Front-end/js/site.js
@@ -52,4 +52,74 @@ $(document).ready(function () {
             data: {places: Array.from(places.values())}
         });
     });
+
+
+    $('.reg-form #email').on("keyup", function () {
+        var value = $(this).val()
+
+        var regex_res = value.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/gi);
+        if (value && regex_res === null) {
+            $('.email-error').html('Введите правильный E-mail адрес');
+
+            addDanger(this)
+        } else {
+            $('.email-error').html('');
+            addSuccess(this)
+        }
+    });
+
+    $('.reg-form #password').on("keyup", function () {
+        var value = $(this).val()
+
+        if (value.length < 6) {
+            $('.password-error').html('Пароль должен быть не менее 6 символов в длину');
+            addDanger(this);
+        } else {
+            $('.password-error').html('');
+            addSuccess(this)
+        }
+        $('.reg-form #repeat-password').keyup();
+
+    });
+    $('.reg-form #repeat-password').on("keyup", function () {
+        var value = $(this).val();
+        var password = $('#password').val();
+
+        if (value !== password) {
+            $('.repeat-password-error').html('Пароли не совпадают');
+            addDanger(this)
+        } else {
+            $('.repeat-password-error').html('');
+            addSuccess(this)
+        }
+    });
+    $('.reg-form #phone').on('keyup', function () {
+        var value = $(this).val();
+
+        if (value && value.match(/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/) === null) {
+            addDanger(this);
+            $('.phone-error').html('Введите правильный номер телефона');
+        } else {
+            addSuccess(this);
+            $('.phone-error').html('');
+        }
+    });
 });
+
+function addDanger(el) {
+    if (!$(el).hasClass('border-danger'))
+        $(el).addClass('border-danger');
+    $(el).removeClass('border-success');
+
+    $('.reg-form #submit').prop('disabled', true)
+}
+
+function addSuccess(el) {
+    $(el).removeClass('border-danger');
+    if (!$(el).hasClass('border-success'))
+        $(el).addClass('border-success');
+
+    if ($('.border-danger').length === 0) {
+        $('.reg-form #submit').prop('disabled', false)
+    }
+}
